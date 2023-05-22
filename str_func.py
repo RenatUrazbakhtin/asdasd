461b93dad7dc36bdb6c93d59ef6aa0d51df2a295
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-05-22 16:49:31 +0300

    changes str_func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,12 @@
 def upper_str(str):
+    """
+    верхний регистр
+    """
     return str.upper()
+
+def each_word_capitalize(str):
+    """
+    Первые буквы строки заглавные
+    """
+    return str.title()
 
